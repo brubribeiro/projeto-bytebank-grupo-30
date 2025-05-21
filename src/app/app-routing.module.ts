92313ce476abd4c603df81b7223d7e7a4f805263
Herbert Ferreira <hbtferreira@hotmail.com>
2025-05-20 21:42:33 -0300

    Remove importação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import path from 'path';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 
