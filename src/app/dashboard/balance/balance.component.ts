5ac38fc46b98d8afb6820ff9f3b6a8bd0bd4b9ec
andersonlimadev <andersonlimahw@gmail.com>
2025-05-31 16:39:27 -0300

    🆕 feature: adiconando observables para transactionServices

--- src/app/dashboard/balance/balance.component.ts
+++ src/app/dashboard/balance/balance.component.ts
@@ -3,15 +3,15 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-balance',
   templateUrl: './balance.component.html',
-  styleUrl: './balance.component.scss'
+  styleUrl: './balance.component.scss',
 })
 export class BalanceComponent {
   @Input() saldo = 0;
   @Input() nome = 'Joana da Silva';
-  
+
   hoje = new Date();
   saldoVisivel = true;
-  
+
   get nomeCurto() {
     return this.nome.split(' ')[0];
   }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TransactionService } from '../services/transaction.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
   nome = 'Joana da Silva Oliveira';
-  saldo = 500;
+  saldo: number = 0;
 
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit(): void {
+    this.transactionService.getBalance().subscribe({
+      next: (balance) => (this.saldo = balance),
+      error: (err) => console.error('Erro ao carregar saldo:', err),
+    });
+  }
 }
