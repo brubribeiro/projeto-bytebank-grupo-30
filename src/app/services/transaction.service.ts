dc9c81108bd9714e87c4371370bfea09b07f7d2a
andersonlimadev <andersonlimahw@gmail.com>
2025-05-31 16:39:27 -0300

    🆕 feature: adiconando observables para transactionServices

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Transaction } from '../models/transaction.model';
 import { BaseService } from './base.service';
+import { Observable, firstValueFrom } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class TransactionService extends BaseService<Transaction> {
@@ -12,5 +14,18 @@ export class TransactionService extends BaseService<Transaction> {
   getByAccount() {
     console.log('Fetching transactions for the account');
     return this.findAll();
+  }
+
+  getBalance(): Observable<number> {
+    return this.getByAccount().pipe(
+      map((transactions: Transaction[]) =>
+        transactions.reduce((total, t) => total + t.value, 0)
+      )
+    );
+  }
+
+  async getBalanceSync(): Promise<number> {
+    const transactions = await firstValueFrom(this.getByAccount());
+    return transactions.reduce((total, t) => total + t.value, 0);
   }
 }
