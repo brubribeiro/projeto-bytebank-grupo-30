b94bac802a085873f731c25cac38c39267529e24
Herbert Ferreira <herbert.ferreira@axulus.io>
2025-05-31 20:38:46 -0300

    Implementa menu e ajustes de layout para alinhamento

--- src/app/dashboard/header-dashboard/header-dashboard.component.ts
+++ src/app/dashboard/header-dashboard/header-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { MenuItem } from '../../interfaces/menu-item.interface';
 
 @Component({
   selector: 'app-header-dashboard',
@@ -8,7 +9,10 @@ import { Component, Input } from '@angular/core';
 export class HeaderDashboardComponent {
   @Input() nome = 'Joana da Silva';
 
-  toggleSidenav(): void {
-    
-  }
+  menuItems: MenuItem[] = [
+    { label: 'Início', route: 'dashboard/#', active: true },
+    { label: 'Transferências', route: 'dashboard/#', active: false },
+    { label: 'Investimentos', route: 'dashboard/#', active: false },
+    { label: 'Outros Serviços', route: 'dashboard/#', active: false }
+  ];
 }
