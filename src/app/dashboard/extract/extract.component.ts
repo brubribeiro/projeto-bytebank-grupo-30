bea4da31b24d7bb22f141dd59b3bcf60cec49857
andersonlimadev <andersonlimahw@gmail.com>
2025-05-31 15:41:11 -0300

    🆕 feature: adding extract component

--- src/app/dashboard/extract/extract.component.ts
+++ src/app/dashboard/extract/extract.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface ExtratoItem {
+  mes: string;
+  tipo: string;
+  valor: number;
+  data: string;
+}
+
 @Component({
   selector: 'app-extract',
   templateUrl: './extract.component.html',
-  styleUrl: './extract.component.css'
+  styleUrls: ['./extract.component.scss'],
 })
 export class ExtractComponent {
-
+  extrato: ExtratoItem[] = [
+    { mes: 'Novembro', tipo: 'Depósito', valor: 150, data: '18/11/2022' },
+    { mes: 'Novembro', tipo: 'Depósito', valor: 100, data: '21/11/2022' },
+    { mes: 'Novembro', tipo: 'Depósito', valor: 50, data: '21/11/2022' },
+    { mes: 'Novembro', tipo: 'Transferência', valor: -500, data: '21/11/2022' },
+  ];
 }
