db6c02b27563d1acb5fbd66744bf63f2f4d840cb
Bruna Ribeiro <bruna.ribeiro@facens.br>
2025-05-31 22:44:52 -0300

    fix(header-home): refatorado header desktop e tablet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  useLocalDb: false,
   apiBaseUrl: 'http://localhost:3003/api',
   apiVersion: '/v1',
-  apiNamespace: '/bytebank',
+  apiNamespace: '/bytebank'
 };
