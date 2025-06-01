7901d5838f9e0840980495f8850e77d7b5077417
andersonlimadev <andersonlimahw@gmail.com>
2025-05-31 21:11:47 -0300

    🆕 feature: integrando account-service com ui

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -2,10 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Account } from '../models/account.model';
 import { BaseService } from './base.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AccountService extends BaseService<Account> {
+  private accountSubject = new BehaviorSubject<Account | null>(null);
+  account$ = this.accountSubject.asObservable();
+
   constructor(http: HttpClient) {
-    super(http, 'account');
+    super(http, 'accounts');
+  }
+
+  loadAccount(): Observable<Account> {
+    return this.findById('1').pipe(
+      tap((account) => this.accountSubject.next(account))
+    );
+  }
+
+  getAccountSnapshot(): Account | null {
+    return this.accountSubject.getValue();
   }
 }
